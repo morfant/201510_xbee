beded654a7fff6479d35096a9fc4f5d195e41912
Gangil <git>
2015-10-30 17:56:35 +0900

    Refinement.

---
+++ Series1_TxRx_Teensy30/Series1_TxRx_Teensy30.ino
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2009 Andrew Rapp. All rights reserved.
+ *
+ * This file is part of XBee-Arduino.
+ *
+ * XBee-Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XBee-Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XBee-Arduino.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <XBee.h>
+#define MODULE_NUM 2 // Set module number
+
+
+#define HWSERIAL Serial3 //Teensy 3.0 only
+#define DEBUG
+#define STARTUP_DELAY 15000
+
+uint8_t senderID[] = {'A', 'B', 'C'};
+uint8_t distID[] = {1, 2, 3};
+uint16_t addr_dest[]= {0x0A0A, 0x1B1B, 0x2C2C, 0x3D3D};
+
+XBee xbee = XBee();
+
+// TX
+uint8_t payload[] = { senderID[MODULE_NUM] };
+uint8_t payloadToC[] = { senderID[MODULE_NUM], 0 };
+
+Tx16Request tx = Tx16Request(addr_dest[MODULE_NUM], payload, sizeof(payload)); // to next module
+Tx16Request txC = Tx16Request(addr_dest[3], payloadToC, sizeof(payloadToC)); // to center module
+
+
+// RX
+XBeeResponse response = XBeeResponse();
+// create reusable response objects for responses we expect to handle 
+Rx16Response rx16 = Rx16Response();
+Rx64Response rx64 = Rx64Response(); // Is it Needed?
+
+// IO pins for Teensy 3.0
+int txLed = 13; // 11 for Teensy 2.0
+int rxLed = 16; // 12 for Teensy 2.0
+int but = 0;
+
+
+// Variables
+uint8_t getSenderID = 0;
+uint8_t rssi = 0;
+unsigned long nextTime = 0;
+
+
+// Functions
+void flashLed(int pin, int times, int wait) {
+    for (int i = 0; i < times; i++) {
+        digitalWrite(pin, HIGH);
+        delay(wait);
+        digitalWrite(pin, LOW);
+      
+        if (i + 1 < times) {
+            delay(wait);
+        }
+    }
+}
+
+
+// Setup
+void setup() {
+    pinMode(txLed, OUTPUT);
+    pinMode(rxLed, OUTPUT);
+    pinMode(but, INPUT_PULLUP);
+  
+    //start USB serial  
+    Serial.begin(9600);
+  
+    HWSERIAL.begin(57600);
+    xbee.setSerial(HWSERIAL);
+  
+    // Start signal
+    flashLed(txLed, 1, 50);
+    flashLed(rxLed, 1, 50);    
+    flashLed(txLed, 1, 50);    
+}
+
+
+
+// continuously reads packets, looking for RX16 or RX64
+void loop() {
+    delay(STARTUP_DELAY); // Is it needed?
+
+    while(1){
+      
+      // Receive from previous module
+      xbee.readPacket();
+      
+        if (xbee.getResponse().isAvailable()) {
+            if (xbee.getResponse().getApiId() == RX_16_RESPONSE || xbee.getResponse().getApiId() == RX_64_RESPONSE) {
+
+#ifdef DEBUG
+            // got something
+            Serial.println("Serial datas are comming.");            
+#endif
+            getSenderID = 0;
+            rssi = 0;
+
+            // got a rx packet
+            if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+              flashLed(rxLed, 1, 10);                  
+              xbee.getResponse().getRx16Response(rx16);
+              getSenderID = rx16.getData(0);
+              rssi = rx16.getRssi();
+            } else {
+              flashLed(rxLed, 1, 10);                                
+              xbee.getResponse().getRx64Response(rx64);
+              getSenderID = rx64.getData(0);
+              rssi = rx64.getRssi();        
+            }
+
+
+#ifdef DEBUG
+            Serial.print("senderID: "); Serial.println(getSenderID); //received as Decimal Number
+            Serial.print("RSSI: "); Serial.println(rssi);
+            Serial.print("\n");        
+#endif
+            }else if(xbee.getResponse().isError()) {
+              Serial.println("Error reading packet.  Error code: ");  
+              Serial.println(xbee.getResponse().getErrorCode());
+            }
+        };
+
+
+// Send to Center module
+
+        if (getSenderID == senderID[(MODULE_NUM+2)%3]){
+            // Receive msg from previous module.
+            payloadToC[0] = distID[MODULE_NUM];
+            payloadToC[1] = rssi;
+            
+        }else{
+            // Didn't receive msg from previous module.
+            payloadToC[0] = distID[MODULE_NUM];
+            payloadToC[1] = 0;
+        }
+    
+       // Buttom push check 
+       if(!digitalRead(but)){
+           // Do something special.
+        }
+
+        xbee.send(txC);
+        flashLed(txLed, 1, 10);
+
+
+// Send to next module
+        payload[0] = senderID[MODULE_NUM];
+        xbee.send(tx);
+        flashLed(txLed, 1, 10);
+   }
+}
